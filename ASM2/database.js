2577939b258bdaacaf6dc905a460c6517b6fbc46
Zeeto265 <87642003+Zeeto265@users.noreply.github.com>
2021-08-25 14:47:25 +0700

    ok

--- ASM2/database.js
+++ ASM2/database.js
@@ -1,5 +1,6 @@
 const {ObjectId,MongoClient} = require('mongodb');
-const url = 'mongodb+srv://asm2-1644:nam2652001@cluster0.vqr6y.mongodb.net/test';
+//const url = 'mongodb://localhost:27017';
+const url = 'mongodb+srv://asm1644:nam2652001@cluster0.vqr6y.mongodb.net/test';
 
 async function getDB() {
     const client = await MongoClient.connect(url);
